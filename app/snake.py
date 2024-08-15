5cf98042aa5ff80e35b75deaba21fd306c84dde7
Greg Thorson <Engywook2005@gmail.com>
2024-08-14 22:40:18 -0400

    Doing original positioning of the snake OK

---
+++ app/snake.py
@@ -0,0 +1,35 @@
+RIGHT = (1, 0)
+LEFT = (-1, 0)
+UP = (0, -1)
+DOWN = (0, -1)
+
+class Snake:
+    def __init__(self, field, sprite, length = 2):
+        self.field = field
+        self.sprite = sprite
+        self.length = length
+        self.direction = RIGHT
+        self.positions = self.set_initial_positions()
+
+    def set_initial_positions(self):
+        x_values = []
+        y_values = []
+
+        current_x = self.length if self.direction[0] == 1 else self.field[0] - self.length
+        current_y = self.length if self.direction[1] == 1 else self.field[1] - self.length
+
+        step_x = self.direction[0]
+        step_y = self.direction[1]
+
+        n = 0
+
+        while n < self.length:
+            current_x = current_x - step_x
+            current_y = current_y - step_y
+
+            x_values.append(current_x)
+            y_values.append(current_y)
+
+            n += 1
+
+        return zip(tuple(x_values), tuple(y_values))        
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from app.apple import Apple
+from app.snake import Snake
 
-occupied_positions = []
+# Todo move to controller class that has a loop to keep the game going.
+
+occupied_positions = [(0,0)]
 
 field = (11, 11)
 
-apple = Apple((field), None)
+apple = Apple(field, None)
+snake = Snake(field, None)
+
+print(tuple(snake.positions))
 
 new_position = apple.spawn(occupied_positions).position
+occupied_positions.append(new_position)
 
-print(new_position)
+# print(new_position)
+# print(occupied_positions)
